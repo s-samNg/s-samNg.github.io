3caa5cafd8b013e978429212412a5992a999b4ba
s-samNg <76829073+s-samNg@users.noreply.github.com>
2022-03-10 05:59:32 +0100

    Template v2
    Template v2.

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,24 @@
-let createDial = (i) =>{  
+let createDial = (i) => {
     let div = document.createElement('div');
-    let container = document.getElementById('container');     
-    container.appendChild(div);             
-    div.classList.add('button');  
-    div.textContent= i;           
-  }
-  for(let i = 0; i < 10; i++)
+    let container = document.getElementById('container');
+    container.appendChild(div);
+    div.classList.add('button');
+    div.textContent = i;
+}
+for (let i = 0; i < 10; i++)
     createDial(i);
+
+
+
+const createCode = (codeLength) => {
+    let count = 0;
+    let code = [];
+    for (let i = 0; i < codeLength; i++) 
+    {
+        code.push(Math.floor(Math.random() * 10));
+    }
+    return code;
+}
+code = createCode(4);
+codeString = code.join(''); 
+console.log(`code secret => ${codeString}`);
